7dabb0854d683e3fcef1d187095776d4c89439d8
Ter-web <n.terfa@yahoo.com>
2023-11-27 12:25:34 +0200

    Refactor the script file

--- script.js
+++ script.js
@@ -1,61 +1,43 @@
 const notesContainer = document.querySelector('.notes-container');
-const createBtn = document.querySelector('.btn');
+const createBtn = document.querySelector('.btn')
 
-let staleNotes = localStorage.getItem('notes')
-window.addEventListener('load', () => {
- 
-  if(staleNotes.length > 0) {
-    renderPage()
-    
-  }
-})
-
-function renderPage() {
-  
-  let inputBox = document.createElement('p');
-  let img = document.createElement('img');
-  inputBox.className = 'input-box';
-  inputBox.setAttribute('contenteditable', 'true');
-  img.src = 'images/delete.png';
-  inputBox.textContent = staleNotes
-  notesContainer.appendChild(inputBox).appendChild(img);
-  
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem('notes');
 }
+
+showNotes();
 function updateStorage(){
-  localStorage.setItem('notes', notesContainer.textContent)
+  localStorage.setItem('notes', notesContainer.innerHTML)
 }
-
-let notes = document.querySelectorAll('.input-box')
+let notes = document.querySelectorAll('.input-box');
 
 createBtn.addEventListener('click', () => {
-  renderNewPage()
-  
-  
-})
-
-function renderNewPage() {
   let inputBox = document.createElement('p');
   let img = document.createElement('img');
   inputBox.className = 'input-box';
   inputBox.setAttribute('contenteditable', 'true');
   img.src = 'images/delete.png';
   notesContainer.appendChild(inputBox).appendChild(img);
-}
-notesContainer.addEventListener('click', function(e){
-  if(e.target.tagName === 'IMG') {
-    updateStorage();
-    e.target.parentElement.remove();
-    
-  }
-  else if(e.target.tagName === 'P'){
-    notes = document.querySelectorAll('.input-box');
+  inputBox.focus();
+})
 
+notesContainer.addEventListener('click', (e) => {
+  if(e.target.tagName === 'IMG') {
+    e.target.parentElement.remove();
+    updateStorage();
+  }else if (e.target.tagName === 'P'){
+    notes = document.querySelectorAll('.input-box');
     notes.forEach(note => {
-      note.onkeyUp = function() {
-          updateStorage()
+      note.onkeyup = function() {
+        updateStorage();
       }
     })
   }
 })
 
-
+document.addEventListener('keydown', event => {
+  if(event.key === 'Enter'){
+    document.execCommand('insertLineBreak');
+    event.preventDefault();
+  }
+})
