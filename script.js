1a1b4987a6c08ba3eb3418612f785439cd14e483
Ter-web <n.terfa@yahoo.com>
2023-11-27 11:47:38 +0200

    refactor the script file

--- script.js
+++ script.js
@@ -58,8 +58,4 @@ notesContainer.addEventListener('click', function(e){
   }
 })
 
-document.addEventListener('keydown', event => {
-  if(event.key === 'Enter')
-    document.execCommand('insertLineBreak');
-    event.preventDefault();
-})
+
